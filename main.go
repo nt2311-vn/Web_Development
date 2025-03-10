b0fdf3888d0ff8a1f91d87254cc394a61104302a
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-10 15:17:40 +0700

    serve file static

--- main.go
+++ main.go
@@ -5,12 +5,15 @@ import (
 	"html"
 	"log"
 	"net/http"
+	"path/filepath"
 	"time"
 )
 
 func main() {
 	http.HandleFunc("/", showInfo)
-	http.HandleFunc("/site", serveFile)
+
+	files := http.FileServer(http.Dir(filepath.Join("var", "www")))
+	http.Handle("/site/", http.StripPrefix("/site/", files))
 
 	if err := http.ListenAndServe(":8999", nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
@@ -20,8 +23,4 @@ func main() {
 func showInfo(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Current time: ", time.Now())
 	fmt.Fprintln(w, "URL Path: ", html.EscapeString(r.URL.Path))
-}
-
-func serveFile(w http.ResponseWriter, r *http.Request) {
-	http.ServeFile(w, r, "imdex.html")
 }
