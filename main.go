73f75707b860342d2f0553923b80c1b0cf43f122
nt2311-vn <nguyentoan231196@gmail.com>
2025-03-10 15:08:31 +0700

    default serve mux with go standard net/http pacakge

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
 import (
+	"fmt"
+	"html"
+	"log"
 	"net/http"
-	"path/filepath"
+	"time"
 )
 
 func main() {
-	http.ListenAndServe(":8999", http.FileServer(http.Dir(filepath.Join("var", "www"))))
+	http.HandleFunc("/", showInfo)
+	http.HandleFunc("/site", serveFile)
+
+	if err := http.ListenAndServe(":8999", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func showInfo(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Current time: ", time.Now())
+	fmt.Fprintln(w, "URL Path: ", html.EscapeString(r.URL.Path))
+}
+
+func serveFile(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "imdex.html")
 }
